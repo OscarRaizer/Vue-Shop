c105e48e2d9200b999a041dfc088c7bd6c83f392
Oscar <boltenkovs36@gmail.com>
2024-04-21 23:43:12 +0700

    init: init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <div class="m-10 bg-red-90 text-white">
+    <MyHello title="Adidas" have :price="12" class="m-100">
+      <button>click me!</button>
+    </MyHello>
+  </div>
+</template>
+
+<style>
+#app {
+  display: flex;
+  gap: 30px;
+}
+</style>
